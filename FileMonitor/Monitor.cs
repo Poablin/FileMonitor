ab455d0803ff1f807ad540e4f4e6961334b839c1
Poablin <65161335+Poablin@users.noreply.github.com>
2020-12-16 18:06:41 +0100

    Initial commit

---
+++ FileMonitor/Monitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileMonitor
+{
+    internal class Monitor
+    {
+        public Monitor(string path)
+        {
+            Path = path;
+        }
+        private string Path;
+
+        public void SearchThroughFiles()
+        {
+            var files = Directory.GetFiles(Path);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
---
+++ FileMonitor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monitor = new Monitor(@"C:\Users\krist\Downloads\test\Done\20201209");
+            monitor.SearchThroughFiles();
+            while(true)
+            {
+
+            }
+        }
+    }
+}
