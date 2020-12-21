8b45d3fa9452a5636c9bff5680b701cbac6999f3
Poablin <65161335+Poablin@users.noreply.github.com>
2020-12-21 12:04:42 +0100

    La til try catcher

--- FileMonitor/FileDeletionService.cs
+++ FileMonitor/FileDeletionService.cs
@@ -18,39 +18,53 @@ namespace FileMonitor
 
         public void Run()
         {
-            foreach (var path in _paths)
+            try
             {
-                if (!_fileSystemValidator.TryGetDoneFolder(path, out var doneFolder))
+                foreach (var path in _paths)
                 {
-                    _logger.Log("Done folder does not exist");
-                    continue;
-                }
-
-                var subFolders = doneFolder.EnumerateDirectories()
-                    .Where(x => _fileSystemValidator.DirectoryIsValid(x.Name));
-
-                foreach (var subFolder in subFolders)
-                {
-                    var filesToDelete = subFolder.EnumerateFiles()
-                        .Where(x => _fileSystemValidator.FileIsValid(x.Name));
-
-                    foreach (var fileToDelete in filesToDelete)
+                    if (!_fileSystemValidator.TryGetDoneFolder(path, out var doneFolder))
                     {
-                        _logger.Log("File: " + fileToDelete.Name + " - Deleted");
-                        fileToDelete.Delete();
+                        _logger.Log("Done folder does not exist");
+                        continue;
                     }
 
-                    DeleteFolderIfEmpty(subFolder);
+                    var subFolders = doneFolder.EnumerateDirectories()
+                        .Where(x => _fileSystemValidator.DirectoryIsValid(x.Name));
+
+                    foreach (var subFolder in subFolders)
+                    {
+                        var filesToDelete = subFolder.EnumerateFiles()
+                            .Where(x => _fileSystemValidator.FileIsValid(x.Name));
+
+                        foreach (var fileToDelete in filesToDelete)
+                        {
+                            _logger.Log("File: " + fileToDelete.Name + " - Deleted");
+                            fileToDelete.Delete();
+                        }
+
+                        DeleteFolderIfEmpty(subFolder);
+                    }
                 }
+            }
+            catch (IOException e)
+            {
+                _logger.Log(e.ToString());
             }
         }
 
         public void DeleteFolderIfEmpty(DirectoryInfo subFolder)
         {
-            if (!subFolder.EnumerateFiles().Any())
+            try
             {
-                _logger.Log("Folder at " + subFolder.FullName + " - Deleted");
-                subFolder.Delete();
+                if (!subFolder.EnumerateFiles().Any())
+                {
+                    _logger.Log("Folder at " + subFolder.FullName + " - Deleted");
+                    subFolder.Delete();
+                }
+            }
+            catch (IOException e)
+            {
+                _logger.Log(e.ToString());
             }
         }
     }
