a92a7e5452f36abb1367730d64c2b8bbf692e3b1
Poablin <65161335+Poablin@users.noreply.github.com>
2020-12-21 17:02:13 +0100

    Update FileSystemValidator.cs

--- FileMonitor/Utilities/FileSystemValidator.cs
+++ FileMonitor/Utilities/FileSystemValidator.cs
@@ -24,7 +24,9 @@ namespace FileMonitor.Utilities
         public bool FileIsValid(string fileName)
         {
             var isCorrectFormat = Regex.IsMatch(fileName, @"^.*[.]\[IM-\d+]-\[(?<deleteDate>\d{12})]$");
-            var fileDate = isCorrectFormat ? fileName.Substring(fileName.LastIndexOf('[')).Trim('[', ']') : string.Empty;
+            var fileDate = isCorrectFormat
+                ? fileName.Substring(fileName.LastIndexOf('[')).Trim('[', ']')
+                : string.Empty;
             var isValidDate = DateTime.TryParseExact(fileDate, "yyyyMMddHHmm", null, DateTimeStyles.AssumeLocal,
                 out var validDate);
             return isCorrectFormat && isValidDate && validDate < DateTime.Now;
