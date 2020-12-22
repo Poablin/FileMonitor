6eaf9b5e148c79584d9caa020cddb388d72ae7b0
Poablin <65161335+Poablin@users.noreply.github.com>
2020-12-22 11:59:44 +0100

    Update FileDeletionService.cs

--- FileMonitor/FileDeletionService.cs
+++ FileMonitor/FileDeletionService.cs
@@ -21,31 +21,7 @@ namespace FileMonitor
         {
             try
             {
-                foreach (var path in _paths)
-                {
-                    if (!_fileSystemValidator.TryGetDoneFolder(path, out var doneFolder))
-                    {
-                        _logger.Log("Done folder does not exist");
-                        continue;
-                    }
-
-                    var subFolders = doneFolder.EnumerateDirectories()
-                        .Where(x => _fileSystemValidator.DirectoryIsValid(x.Name));
-
-                    foreach (var subFolder in subFolders)
-                    {
-                        var filesToDelete = subFolder.EnumerateFiles()
-                            .Where(x => _fileSystemValidator.FileIsValid(x.Name));
-
-                        foreach (var fileToDelete in filesToDelete)
-                        {
-                            fileToDelete.Delete();
-                            _logger.Log($"File: {fileToDelete.Name} - Deleted");
-                        }
-
-                        DeleteFolderIfEmpty(subFolder);
-                    }
-                }
+                DeleteExpiredFilesAndEmptyFolders(_paths);
             }
             catch (Exception e)
             {
@@ -53,12 +29,48 @@ namespace FileMonitor
             }
         }
 
+        private void DeleteExpiredFilesAndEmptyFolders(string[] _paths)
+        {
+            foreach (var path in _paths)
+            {
+                if (!_fileSystemValidator.TryGetDoneFolder(path, out var doneFolder))
+                {
+                    _logger.Log("Done folder does not exist");
+                    continue;
+                }
+
+                var subFolders = doneFolder.EnumerateDirectories()
+                    .Where(x => _fileSystemValidator.DirectoryIsValid(x.Name));
+
+                foreach (var subFolder in subFolders)
+                {
+                    var filesToDelete = subFolder.EnumerateFiles()
+                        .Where(x => _fileSystemValidator.FileIsValid(x.Name));
+
+                    foreach (var fileToDelete in filesToDelete)
+                    {
+                        fileToDelete.Delete();
+                        _logger.Log($"File: {fileToDelete.Name} - Deleted");
+                    }
+
+                    DeleteFolderIfEmpty(subFolder);
+                }
+            }
+        }
+
         private void DeleteFolderIfEmpty(DirectoryInfo subFolder)
         {
             if (subFolder.EnumerateFiles().Any()) return;
 
-            subFolder.Delete();
-            _logger.Log($"Folder at {subFolder.FullName} - Deleted");
+            try
+            {
+                subFolder.Delete();
+                _logger.Log($"Folder at {subFolder.FullName} - Deleted");
+            }
+            catch (Exception e)
+            {
+                _logger.Log(e.Message);
+            }
         }
     }
 }
